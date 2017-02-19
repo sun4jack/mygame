a1bf8bdb1c459f76709167f2178e8bb951f940d9
sun4jack <sun_yongjun@hotmail.com>
2017-02-19 14:55:28 +0800

    YiYi played with the ball

--- js/html5games.pingpong.js
+++ js/html5games.pingpong.js
@@ -1,13 +1,40 @@
 var KEY = {
+BALLLEFT:74,
+BALLRIGHT:76,
+BALLUP: 73,
+BALLDOWN: 75,
 UP: 38,
 DOWN: 40,
 W: 87,
 S: 83
 }
+
 $(function(){
 // listen to the key down event
 $(document).keydown(function(e){
 switch(e.which){
+case KEY.BALLUP: // B pressed
+// get the current Ball's top value in Int type
+var top = parseInt($("#ball").css("top"));
+// move the ball up 5 pixels
+$("#ball").css("top",top-5);
+break;
+case KEY.BALLDOWN: // space pressed
+var top = parseInt($("#ball").css("top"));
+// move the ballle B down 5 pixels
+$("#ball").css("top",top+5);
+break;
+case KEY.BALLLEFT: // V pressed
+// get the current Ball's top value in Int type
+var yleft = parseInt($("#ball").css("left"));
+// move the ball up 5 pixels
+$("#ball").css("left",yleft-5);
+break;
+case KEY.BALLRIGHT: // N pressed
+var yleft = parseInt($("#ball").css("left"));
+// move the ballle B down 5 pixels
+$("#ball").css("left",yleft+5);
+break;
 case KEY.UP: // arrow-up
 // get the current paddle B's top value in Int type
 var top = parseInt($("#paddleB").css("top"));
